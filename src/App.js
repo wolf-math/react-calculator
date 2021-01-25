692ad76e4f26f79f18ff71d8d3457ac6e4f0996f
aaron <aaron.wolf@gmail.com>
2021-01-25 14:25:04 +0200

    moving logic up from screen to app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
+import React from 'react';
+
 import './App.css';
 import Screen from './components/screen/screen';
 import Numpad from './components/numpad/numpad';
 
 
-function App() {
-  return (
-    <div className="App">
-      <Screen />
-      <Numpad />
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      submit: '0'
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      // input: event.target.value
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.setState({
+      submit: this.state.input
+    });
+  }
+
+  result = () => {
+      try {
+          return eval(this.state.submit)
+      } catch(e) {
+          return "ERROR"
+      }
+  }
+
+  render() {
+      return (
+        <div>
+          <Screen onChange={this.handleChange()} onSubmit={this.handleSubmit()} />
+          <Numpad onChange={this.handleChange()}/>
+        </div>
+      );
+  }
+};
 
 export default App;
--- src/components/screen/screen.jsx
+++ src/components/screen/screen.jsx
@@ -1,50 +1,63 @@
+import { render } from '@testing-library/react';
 import React from 'react';
 
 import './screen.scss';
 
-class Screen extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        input: '',
-        submit: '0'
-      };
-    }
+// class Screen extends React.Component {
+//     constructor(props) {
+//       super(props);
+//       this.state = {
+//         input: '',
+//         submit: '0'
+//       };
+//     }
 
-    handleChange = (event) => {
-      this.setState({
-        input: event.target.value
-      });
-    }
+//     handleChange = (event) => {
+//       this.setState({
+//         input: event.target.value
+//       });
+//     }
 
-    handleSubmit = (event) => {
-      event.preventDefault()
-      this.setState({
-        submit: this.state.input
-      });
-    }
+//     handleSubmit = (event) => {
+//       event.preventDefault()
+//       this.setState({
+//         submit: this.state.input
+//       });
+//     }
 
-    result = () => {
-        try {
-            return eval(this.state.submit)
-        } catch(e) {
-            return "ERROR"
-        }
-    }
+//     result = () => {
+//         try {
+//             return eval(this.state.submit)
+//         } catch(e) {
+//             return "ERROR"
+//         }
+//     }
 
-    render() {
-        return (
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    <input
-                        value={this.state.input}
-                        onChange={this.handleChange} />
-                    <button type='submit'>=</button>
-                </form>
-                <h1>{this.result()}</h1>
-            </div>
-        );
-    }
-};
-  
+//     render() {
+//         return (
+//             <div>
+//                 <form onSubmit={this.handleSubmit}>
+//                     <input
+//                     value={this.state.input}
+//                     onChange={this.handleChange} />
+//                     <button type='submit'>=</button>
+//                 </form>
+//                 <h1>{this.result()}</h1>
+//             </div>
+//         );
+//     }
+// };
+
+const Screen = (props) => {
+    return (
+        <div>
+            <form onSubmit={this.handleSubmit}>
+                <input value={this.state.input} onChange={this.handleChange} />
+                <button type='submit'>=</button>
+            </form>
+            <h1>{this.result()}</h1>
+        </div>
+    )
+}
+
 export default Screen;
